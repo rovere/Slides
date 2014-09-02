131d3f229a7c350a9bc4a2e486f9a7b738e1415e
Marco Rovere <marco.rovere@cern.ch>
2014-09-02 11:36:16 +0200

    Add setup script to be run as root

---
+++ setupR.r
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+install.packages("RCurl")
+install.packages("devtools")
+devtools::install_github("slidify", "ramnathv")
+devtools::install_github("slidifyLibraries", "ramnathv")
+
